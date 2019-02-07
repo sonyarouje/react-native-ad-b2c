8d8da26938f796f6bb0a8931cedde924ab1b2645
Sony Arouje <sonyarouje@gmail.com>
2019-02-07 12:44:14 +0530

    supports login and refresh token functionality.

---
+++ LoginView.js
@@ -0,0 +1,195 @@
+import React, { Component } from "react";
+import { WebView, Dimensions } from "react-native";
+import log from "./logger";
+import B2CAuthentication from "./ReactNativeADB2C";
+
+export default class LoginView extends React.Component {
+    needRedirect = true;
+    onTokenGranted = () => { };
+    lock = false;
+    accessToken = {};
+
+    constructor(props) {
+        super(props);
+        if (!this.props.context instanceof B2CAuthentication)
+            throw new Error("property `context` of LoginView should be an instance of B2CAuthentication, but got", this.props.context);
+
+        const context = this.props.context;
+        this.needRedirect = this.props.needLogout || false;
+        this.state = {
+            page: this.getLoginUrl(context.getConfig().authority_host),
+            visible: true
+        };
+        this._lock = false;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        if (this.state.visible === nextState.visible && this.state.page === nextState.page)
+            return false;
+        return true;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!this.props.needLogout && nextProps.needLogout) {
+            const context = this.props.context;
+            this.needRedirect = nextProps.needLogout || false;
+            this.setState({
+                page: this.getLoginUrl(context.getConfig().authority_host),
+                visible: true
+            });
+        }
+    }
+
+    render() {
+        // Fix visibility problem on Android webview
+        let js = `document.getElementsByTagName('body')[0].style.height = '${
+            Dimensions.get("window").height
+            }px';`;
+
+        return this.state.visible ? (
+            <WebView
+                ref="ADLoginView"
+                automaticallyAdjustContentInsets={false}
+                style={[
+                    this.props.style,
+                    {
+                        flex: 1,
+                        alignSelf: "stretch",
+                        width: Dimensions.get("window").width,
+                        height: Dimensions.get("window").height
+                    }
+                ]}
+                source={{ uri: this.state.page }}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                onLoadEnd={() => {
+                    if (this._needRedirect) {
+                        this._needRedirect = false;
+                        this.setState({ page: this.getLoginUrl(this.props.context.getConfig().authority_host) });
+                    }
+                }}
+                decelerationRate="normal"
+                javaScriptEnabledAndroid={true}
+                onNavigationStateChange={this._handleADToken.bind(this)}
+                onShouldStartLoadWithRequest={e => {
+                    return true;
+                }}
+                startInLoadingState={false}
+                injectedJavaScript={js}
+                scalesPageToFit={true}
+            />
+        ) : null;
+    };
+
+    /**
+     * Get authority host URI,
+     * @param {string} tenant Custom tenant ID, this filed is optional, default
+     *                        values is `common`.
+     * @return {string} The Authority host URI.
+     */
+    getLoginUrl = (url) => {
+        const authUrl = url; //"https://triguz.b2clogin.com/triguz.onmicrosoft.com/oauth2/v2.0/authorize";
+        const context = this.props.context || null;
+        const redirect = context.getConfig().redirect_uri;
+        const prompt = context.getConfig().prompt;
+        const userFlowPolicy = context.getConfig().user_flow_policy;
+        const clientId = context.getConfig().client_id;
+        const scope = context.getConfig().scope;
+        if (context !== null) {
+            const result =
+                `${authUrl}?p=${userFlowPolicy}&response_type=code` +
+                `&client_id=${clientId}` +
+                (redirect ? `&redirect_uri=${redirect}&nonce=rnad-${Date.now()}` : "") +
+                (prompt ? `&prompt=${prompt}` : "") +
+                (scope ? `&scope=${scope.join(" ")}` : "");
+            ;
+
+            console.log(result);
+            return result;
+        } else {
+            throw new Error("context should not be null/undefined.");
+        }
+    }
+
+    /**
+     * An interceptor for handling webview url change, when it detects possible
+     * authorization code in url, it will triggers authentication flow.
+     * @param  {object} e Navigation state change event object.
+     */
+    _handleADToken(e) {
+        log.verbose("ADLoginView navigate to", e.url);
+        if (this._lock) return true;
+        let code = /((\?|\&)code\=)[^\&]+/.exec(e.url);
+        if (this._needRedirect) {
+            // this._needRedirect = false
+            return true;
+        }
+
+        if (this.props.onURLChange) this.props.onURLChange(e);
+
+        if (code !== null) {
+            this._lock = true;
+            log.verbose("LoginView._handleADToken code=", code[0]);
+            code = String(code[0]).replace(/(\?|\&)?code\=/, "");
+            this.setState({ visible: !this.props.hideAfterLogin });
+            this.props.onVisibilityChange && this.props.onVisibilityChange(false);
+            this._getResourceAccessToken(code).catch(err => {
+                log.error("LoginView._getResourceAccessToken", err);
+            });
+            return true;
+        }
+
+        return true;
+    }
+
+    /**
+ * Get access token for each resoureces
+ * @param {string} code The authorization code from `onNavigationStateChange`
+ *                      callback.
+ * @return {Promise<void>}
+ */
+    _getResourceAccessToken(code) {
+        const context = this.props.context;
+
+        if (!context)
+            throw new Error(
+                "property `context` of LoginView should not be null/undefined"
+            );
+
+        const adConfig = this.props.context.getConfig();
+
+        log.verbose("LoginView get access token for resources");
+
+        let promises = [];
+        const config = {
+            client_id: adConfig.client_id,
+            redirect_uri: adConfig.redirect_uri,
+            code,
+            client_secret: adConfig.client_secret,
+            // set resource to common by default
+            resource: "common"
+        };
+
+        promises.push(context.grantAccessToken("authorization_code", config));
+
+        return Promise.all(promises)
+            .then((resps) => {
+                log.verbose("LoginView response access info ", resps);
+
+                if (!this.props.context)
+                    throw new Error("value of property `context` is invalid=", this.props.context);
+
+                const context = this.props.context;
+                const onSuccess = this.props.onSuccess || function () { };
+
+                // trigger loggined finished event
+                if (context !== null && typeof this.props.onSuccess === "function")
+                    onSuccess(context.getCredentials());
+                this._lock = false;
+            })
+            .catch(err => {
+                log.error(err);
+                throw new Error("Failed to acquire token for resources", err.stack);
+            });
+    }
+}
---
+++ ReactNativeADB2C.js
@@ -0,0 +1,143 @@
+import Timer from "react-timer-mixin";
+import log from "./logger";
+
+const config = {
+  client_secret: "",
+  client_id: "",
+  redirect_uri: "",
+  token_uri: "", //required
+  authority_host: "", //required
+  prompt: "",
+  user_flow_policy: "", //required: e.g. B2C_1_signupsignin,
+  scope: "", //TODO should be an array and while passing seperate with space.
+};
+
+class B2CAuthentication {
+  configuration = {};
+  credentials = {};
+
+  constructor(config) {
+    this.configuration = config;
+  }
+
+  getConfig = () => {
+    return this.configuration;
+  };
+
+  getCredentials = () => {
+    log.verbose("getCredentials", this.credentials);
+    return this.credentials;
+  }
+
+  /**
+  * Assure that access_token is valid, when access token
+  * is expired, this method refresh access token automatically and returns
+  * renewed access token in promise.
+  * @param credentials, pass the credentials recevied at the time of login. credentials data structure should be like
+  * {
+  * "access_token": "eyJ0eXAiOiJKV1QiLCJhb.....",
+  * "expires_in": 3600,
+  * "expires_on": 1549521954,
+  * "not_before": 1549518354,
+  * "profile_info": "eyJ2ZXIiOiIxLjAi....",
+  * "refresh_token": "eyJraWQiOiJjcGltY2....",
+  * "refresh_token_expires_in": 1209600,
+  * "token_type": "Bearer",
+  * }
+  */
+  assureToken = (credentials) => {
+    if (Date.now() < credentials.expires_on)
+      return new Promise.resolve(credentials.access_token);
+    else {
+      return refreshToken(credentials.refresh_token);
+    }
+  };
+
+  refreshToken = (refreshToken) => {
+    return new Promise((resolve, reject) => {
+      if (!refreshToken) reject("Invalid refresh token");
+
+      const config = {
+        refresh_token: refreshToken,
+        client_id: this.configuration.client_id,
+        client_secret: this.configuration.client_secret,
+        resource: "common"
+      }
+
+      const grantType = "refresh_token";
+      this.grantAccessToken(grantType, config)
+        .then((resp) => {
+          resolve(resp.response.access_token);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  };
+
+  grantAccessToken = (grantType, params) => {
+    // If resource is null or undefined, use `common` by default
+    params.resource = params.resource || "common";
+    if (grantType === "password") params["client_id"] = this.config.client_id;
+    return new Promise((resolve, reject) => {
+      try {
+        log.debug(`${grantType} access token for resource ${params.resource}`);
+        var tm = Timer.setTimeout(() => {
+          reject("time out");
+        }, 15000);
+
+        const body = `grant_type=${grantType}${_serialize(params)}`;
+
+        if (!this.configuration.token_uri) reject("token_uri not set in config");
+        log.debug("ReactNativeAD.grantAccessToken: url", this.configuration.token_uri);
+        log.debug("ReactNativeAD.grantAccessToken: body", body);
+
+        fetch(`${this.configuration.token_uri}?p=${this.configuration.user_flow_policy}`, {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          body
+        }).then(response => {
+          Timer.clearTimeout(tm);
+          return response.text();
+        }).then(res => {
+          const cred = {
+            resource: params.resource,
+            response: JSON.parse(res.replace("access_token=", ""))
+          };
+          this.credentials = cred.response;
+
+          if (cred.response.access_token) {
+            log.debug(`received access_token `, cred.response);
+            //   AsyncStorage.setItem(cacheKey, JSON.stringify(cred.response));
+            resolve(cred);
+          } else {
+            log.debug(`failed to grant token`, cred.response);
+            reject(cred);
+          }
+        }).catch(reject);
+
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+
+
+
+}
+
+function _serialize(params) {
+  let paramStr = "";
+  //TODO functionlize the below code.
+  for (let prop in params) {
+    if (params[prop] !== null && params[prop] !== void 0 && prop !== "grant_type")
+      paramStr += `&${prop}=${encodeURIComponent(params[prop])}`;
+  }
+  log.debug("serializes", paramStr);
+  return paramStr;
+}
+
+export default B2CAuthentication;
